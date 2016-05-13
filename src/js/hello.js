308055742d2c1a684818f2cdca0b5b1f10d8625a
nipher <jonas@nipher.io>
2016-05-13 20:46:45 -0300

    Add eslint Replacing jscs and jshint by eslint Fix #13 as well

--- src/js/hello.js
+++ src/js/hello.js
@@ -1,5 +1,7 @@
 'use strict';
 
 export default function hello(name) {
+    /* eslint-disable no-console */
     console.log(`Hello ${name}!`);
+    /* eslint-enable no-console */
 }
--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,14 @@ import hello from './hello';
 // new variable type
 const bla = 'blaaaaaa';
 
+/* eslint-disable no-console */
+
 // template string
 console.log(`Hey ${bla}`);
 
 // spread
 console.log(...[1, 2, 3]);
+
+/* eslint-disable no-console */
 
 hello('Qualy');
